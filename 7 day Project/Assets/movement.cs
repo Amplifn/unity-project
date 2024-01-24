1bace279a50ebc45bd9028580545f0b6894add2c
Amplifn <mrendgamermc@gmail.com>
2024-01-24 16:33:28 +0100

    movement

--- 7 day Project/Assets/movement.cs
+++ 7 day Project/Assets/movement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+
+    public int speed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,34 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.W)) {
+            if (Input.GetKey(KeyCode.A)) {
+                transform.position += new Vector3(speed / -2, speed / 2, 0) * Time.deltaTime;
+            } else if (Input.GetKey(KeyCode.D)) {
+                transform.position += new Vector3(speed / 2, speed / 2, 0) * Time.deltaTime;
+            } else {
+                transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.A)) {
+                transform.position += new Vector3(speed / -2, speed / -2, 0) * Time.deltaTime;
+            } else if (Input.GetKey(KeyCode.D)) {
+                transform.position += new Vector3(speed / 2, speed / -2, 0) * Time.deltaTime;
+            } else {
+                transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.A)) {
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        };
+
     }
 }
