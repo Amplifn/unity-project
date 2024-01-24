17d4b3e4e0aa36bda9bc8938329e6b086ba3c3fa
Blygf <volnyhugo@gmail.com>
2024-01-24 21:03:49 +0100

    updated movememt

--- 7 day Project/Assets/movement.cs
+++ 7 day Project/Assets/movement.cs
@@ -9,14 +9,20 @@ public class movement : MonoBehaviour
 
     public float speed = 0;
     public Sprite[] sprites;
-
+    private bool isCoroutineRunning = false;
+    
     IEnumerator ChangeSprites()
     {
-        for (int i = 1; Input.GetKey(KeyCode.W); i = 3 - i)
+        isCoroutineRunning = true;
+        int i = 1;
+        while (Input.GetKey(KeyCode.W))
         {
             GetComponent<SpriteRenderer>().sprite = sprites[i];
+            i = 3 - i;
             yield return new WaitForSeconds(0.15f);
         }
+        isCoroutineRunning = false;
+        
     }
 
     // Start is called before the first frame update
@@ -30,7 +36,9 @@ public class movement : MonoBehaviour
     {
 
         if (Input.GetKey(KeyCode.W)) {
-            StartCoroutine(ChangeSprites());
+            if (!isCoroutineRunning) {
+                StartCoroutine(ChangeSprites());
+            }
             if (Input.GetKey(KeyCode.A)) {
                 transform.position += new Vector3(-speed * 6 / 10, speed * 6 / 10, 0) * Time.deltaTime;
             } else if (Input.GetKey(KeyCode.D)) {
